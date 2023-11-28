3c3d7374cd3df565c841e71141414c9b740eff3b
RyanDoomes <ryan.doomes@gmail.com>
2023-11-28 18:54:55 -0500

    adding js file

--- snake.js
+++ snake.js
@@ -1,6 +1,40 @@
 //board
-var blocksize = 25;
+var blockSize = 25;
 var rows = 20;
 var cols = 20;
 var board;
 var context;
+
+//snake
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+//food
+var foodX;
+var foodY;
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext('2d');
+
+    placeFood();
+    update(); 
+}
+
+function update() {
+    context.fillStyle="black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="lime"
+    context.fillRect(snakeX, snakeY, blockSize, blockSize)
+
+    context.fillStyle="red"
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
